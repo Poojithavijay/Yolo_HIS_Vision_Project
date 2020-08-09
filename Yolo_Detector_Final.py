45921afcc7077c85803c35261e964b563614bf89
Poojitha Vijayanarasimha <65125317+Poojithavijay@users.noreply.github.com>
2020-08-09 17:32:13 +0530

    Code refactored and built progress bar

--- Yolo_Detector_Final.py
+++ Yolo_Detector_Final.py
@@ -4,24 +4,34 @@ import imutils
 import time
 import cv2
 import os
+from tqdm import tqdm
 from collections import Counter
 
-
+#Accept arguements from user
 ap = argparse.ArgumentParser()
-ap.add_argument("-input", "--input", required=True,
+ap.add_argument("-i", "--input", required=True,
 	help="path to input video")
-ap.add_argument("-output", "--output", required=True,
+ap.add_argument("-o", "--output", required=True,
 	help="path to output video")
-ap.add_argument("-config", "--yolo", required=True,
+ap.add_argument("-y", "--yolo", default="yolo-coco",
 	help="base path to YOLO directory")
-args = vars(ap.parse_args())
-"""
+ap.add_argument("-il", "--inlog", default="logs",
+    help="base path to IN log file")
+ap.add_argument("-ol", "--outlog", default="logs",
+    help="base path to OUT log file")
 ap.add_argument("-c", "--confidence", type=float, default=0.5,
 	help="minimum probability to filter weak detections")
 ap.add_argument("-t", "--threshold", type=float, default=0.3,
-	help="threshold when applyong non-maxima suppression")
-"""
+	help="threshold when applying non-maxima suppression")
+args = vars(ap.parse_args())
 
+# Function to clear temp logs
+def clear_logs():
+	if os.path.exists(os.path.sep.join([args["inlog"], "In.txt"])):
+		os.remove(os.path.sep.join([args["inlog"], "In.txt"]))
+	
+	if os.path.exists(os.path.sep.join([args["outlog"], "Out.txt"])):
+		os.remove(os.path.sep.join([args["outlog"], "Out.txt"]))
 
 # load the COCO class labels our YOLO model was trained on
 labelsPath = os.path.sep.join([args["yolo"], "coco.names"])
@@ -35,22 +45,16 @@ COLORS = np.random.randint(0, 255, size=(len(LABELS), 3), dtype="uint8")
 # derive the paths to the YOLO weights and model configuration
 weightsPath = os.path.sep.join([args["yolo"], "yolov3.weights"])
 configPath = os.path.sep.join([args["yolo"], "yolov3.cfg"])
-"""
-weightsPath = r"D:\yolo-object-detection\yolo-coco\yolov3.weights"
-print("Weights Loaded")
-configPath = r"D:\yolo-object-detection\yolo-coco\yolov3.cfg"
-print("Model Config Loaded")
-"""
 
-# load our YOLO object detector trained on COCO dataset (80 classes)
-# and determine only the *output* layer names that we need from YOLO
-print("[INFO] loading YOLO from disk...")
+# load our YOLO object detector trained on COCO dataset (80 classes) and determine only the *output* layer names that we need from YOLO
+print("\n------------------------------------------------------------------------------------------------------------")
+print("[INFO] Loading Network Weights & Configuration from disk...")
+print("-----------------------------------------------------------------------------------------------------------")
 net = cv2.dnn.readNetFromDarknet(configPath, weightsPath)
 ln = net.getLayerNames()
 ln = [ln[i[0] - 1] for i in net.getUnconnectedOutLayers()]
 
-# initialize the video stream, pointer to output video file, and frame dimensions
-#vs = cv2.VideoCapture(r"D:\yolo-object-detection\videos\overpass.mp4") 
+# initialize the video stream, pointer to output video file, and frame dimensions 
 vs = cv2.VideoCapture(args["input"])
 writer = None
 (W, H) = (None, None)
@@ -60,22 +64,30 @@ try:
 	prop = cv2.cv.CV_CAP_PROP_FRAME_COUNT if imutils.is_cv2() \
 		else cv2.CAP_PROP_FRAME_COUNT
 	total = int(vs.get(prop))
-	print("Total frames in the video: {}".format(total))
+	print("\n-----------------------------------------------------------------------------------------------------------")
+	print("[INFO] Total frames in the video: {}".format(total))
+	print("-----------------------------------------------------------------------------------------------------------")
 
 # an error occurred while trying to determine the total
 # number of frames in the video file
 except:
-	print("[INFO] could not determine # of frames in video")
-	print("[INFO] no approx. completion time can be provided")
+	print("[INFO] Could not determine # of frames in video")
+	print("[INFO] No approximate completion time can be provided")
 	total = -1
 
+#Delete unnecessary log files generated during previous failed execution
+clear_logs()
+
 # loop over frames from the video file stream
-while True:
+#pbar = tqdm(total)
+
+for fr in tqdm(range(total)):
+#while True:
 	# read the next frame from the file
 	(grabbed, frame) = vs.read()
 
 	# if the frame was not grabbed, then we have reached the end
-	# of the stream
+	#pbar.update(1)
 	if not grabbed:
 		break
 
@@ -112,7 +124,7 @@ while True:
 
 			# filter out weak predictions by ensuring the detected
 			# probability is greater than the minimum probability
-			if confidence > 0.5:
+			if confidence > args["confidence"]:
 				# scale the bounding box coordinates back relative to
 				# the size of the image, keeping in mind that YOLO
 				# actually returns the center (x, y)-coordinates of
@@ -134,9 +146,8 @@ while True:
 	#freq = [boxes.count(i) for i in boxes]
 
 
-	# apply non-maxima suppression to suppress weak, overlapping
-	# bounding boxes
-	idxs = cv2.dnn.NMSBoxes(boxes, confidences, 0.3, 0.2)
+	# apply non-maxima suppression to suppress weak, overlapping bounding boxes
+	idxs = cv2.dnn.NMSBoxes(boxes, confidences, args["confidence"], args["threshold"])
 
 	# ensure at least one detection exists
 	if len(idxs) > 0:
@@ -158,7 +169,7 @@ while True:
 			freq1 = [j for j in classIDs]
 			#freq = [[LABELS[classIDs[x]], classIDs.count(x)] for x in set(classIDs)]
 			freq = dict([LABELS[x], classIDs.count(x)] for x in set(classIDs))
-			print("Class:", freq, freq1)
+			#print("Class:", freq, freq1)
 			freq = str(freq)[1:-1]
 			text1 = ("Overall Vehicles in Frame = {}".format(freq))
 			cv2.putText(frame, text1, (10, 50), cv2.FONT_HERSHEY_SIMPLEX, 1, (209, 80, 0, 255), 2)
@@ -197,21 +208,19 @@ while True:
 			#Detect Vehicles in Lane 1 (IN)
 			if (x > 400 and x < 620 and y > 360 and y < 362.5):
 				cv2.line(frame, (415, 361), (615, 361), (0, 0xFF, 0), 7)
-				f = open("D:\yolo-object-detection\In.txt", "a")
+				f = open(os.path.sep.join([args["inlog"], "In.txt"]), "a")
 				f.write("%d\n" %var)
-				print("IN Dump Val:{}".format(var))
+				#print("IN Dump Val:{}".format(var))
 				f.close()
-				#f = open(r"D:\yolo-object-detection\iter.txt", "r")
-				#var1 = f.read()
-				var1 = sum([int(s.strip()) for s in open("D:\yolo-object-detection\In.txt", "r").readlines()])
-				print("IN Read Val:{}".format(var1))
+				var1 = sum([int(s.strip()) for s in open(os.path.sep.join([args["inlog"], "In.txt"]), "r").readlines()])
+				#print("IN Read Val:{}".format(var1))
 				text2 = "{}".format(var1)
 				cv2.putText(frame,text2,(390, 355),cv2.FONT_HERSHEY_SIMPLEX, 0.6, (0, 0, 0xFF), 2)
 				f.close()
 
 			else:
 				try:
-					var1 = sum([int(s.strip()) for s in open("D:\yolo-object-detection\In.txt", "r").readlines()])
+					var1 = sum([int(s.strip()) for s in open(os.path.sep.join([args["inlog"], "In.txt"]), "r").readlines()])
 					text2 = "{}".format(var1)
 					cv2.putText(frame,text2,(390, 355),cv2.FONT_HERSHEY_SIMPLEX,0.6,(0, 0, 0xFF), 2)
 					f.close()
@@ -224,21 +233,19 @@ while True:
 			#Detect Vehicles in Lane 2 (OUT)
 			if (x > 670 and x < 845 and y > 360 and y < 362.5):
 				cv2.line(frame, (670, 361), (843, 361), (0, 0xFF, 0), 7)
-				f = open("D:\yolo-object-detection\Out.txt", "a")
+				f = open(os.path.sep.join([args["outlog"], "Out.txt"]), "a")
 				f.write("%d\n" %var)
-				print("OUT Dump Val:{}".format(var))
+				#print("OUT Dump Val:{}".format(var))
 				f.close()
-				#f = open(r"D:\yolo-object-detection\iter.txt", "r")
-				#var1 = f.read()
-				var1 = sum([int(s.strip()) for s in open("D:\yolo-object-detection\Out.txt", "r").readlines()])
-				print("OUT Read Val:{}".format(var1))
+				var1 = sum([int(s.strip()) for s in open(os.path.sep.join([args["outlog"], "Out.txt"]), "r").readlines()])
+				#print("OUT Read Val:{}".format(var1))
 				text2 = "{}".format(var1)
 				cv2.putText(frame,text2,(895, 355),cv2.FONT_HERSHEY_SIMPLEX, 0.6, (0, 0, 0xFF), 2)
 				f.close()
 
 			else:
 				try:
-					var1 = sum([int(s.strip()) for s in open("D:\yolo-object-detection\Out.txt", "r").readlines()])
+					var1 = sum([int(s.strip()) for s in open(os.path.sep.join([args["outlog"], "Out.txt"]), "r").readlines()])
 					text2 = "{}".format(var1)
 					cv2.putText(frame,text2,(895, 355),cv2.FONT_HERSHEY_SIMPLEX,0.6,(0, 0, 0xFF), 2)
 					f.close()
@@ -261,21 +268,22 @@ while True:
 		# some information on processing single frame
 		if total > 0:
 			elap = (end - start)
-			print("[INFO] single frame took {:.4f} seconds".format(elap))
-			print("[INFO] estimated total time to finish: {:.4f}".format(
-				elap * total))
+			print("\n-----------------------------------------------------------------------------------------------------------")
+			print("[INFO] Estimated time taken to process single frame: {:.4f} seconds".format(elap))
+			print("\n[INFO] Estimated total time to finish object detection: {:.4f} minutes".format((elap * total)/60))
+			print("------------------------------------------------------------------------------------------------------------")
+			print("\n Object Detection Progress: ")
 
 	# write the output frame to disk
 	writer.write(frame)
 
-#Delete unnecessary log files generated
-if os.path.exists("D:\yolo-object-detection\In.txt"):
-	os.remove("D:\yolo-object-detection\In.txt")
-
-if os.path.exists("D:\yolo-object-detection\Out.txt"):
-	os.remove("D:\yolo-object-detection\Out.txt")
+#Delete unnecessary log files generated during current execution
+clear_logs()
 
 # release the file pointers
-print("[INFO] cleaning up...")
+print("\n--------------------------------------------------------------------------------------------------------------------")
+print("[INFO] Cleaning temporary files...")
+print("\n [INFO] Object Detection Complete!")
+print("---------------------------------------------------------------------------------------------------------------------")
 writer.release()
 vs.release()
